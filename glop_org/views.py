bf4df716bf1c6f081355f09aad3a83a54b23dca6
Laurent Raufaste <laurent@percolate.com>
2012-05-21 01:41:17 -0400

    Working version of the index page

--- glop_org/views.py
+++ glop_org/views.py
@@ -1,5 +1,36 @@
+import os
+from google.appengine.ext.webapp import template
 from django.http import HttpResponse
+from random import choice
+from urllib import quote_plus
 
 
-def home(request):
-    return HttpResponse("Hello, world.")
+def index(request):
+    fonts = [
+        'Cantarell',
+        'Cardo',
+        'Crimson Text',
+        'Droid Sans',
+        'Droid Sans Mono',
+        'Droid Serif',
+        'IM Fell DW Pica',
+        'Inconsolata',
+        'Josefin Sans Std Light',
+        'Lobster',
+        'Molengo',
+        'Nobile',
+        'OFL Sorts Mill Goudy TT',
+        'Old Standard TT',
+        'Reenie Beanie',
+        'Tangerine',
+        'Vollkorn',
+        'Yanone Kaffeesatz',
+    ]
+    font = choice(fonts)
+    template_values = {
+        'font': font,
+        'url_encoded_font': quote_plus(font),
+    }
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+
+    return HttpResponse(template.render(path, template_values))
